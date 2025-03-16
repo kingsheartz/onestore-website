9f375f408c262285d7874b816b14e32c1e867f6d
kingsheartz <govind.das279@gmail.com>
2025-03-16 14:56:58 +0530

    updated database details

--- admin/pdo.php
+++ admin/pdo.php
@@ -1,9 +1,17 @@
-<?php
-try{
-	$pdo=new PDO("mysql:host=localhost;port=3306;dbname=OS","root","");
-	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-	$_SESSION['success']="Connected successfully";
-}catch(PDOException $e){
-	$_SESSION['error']="OOPS !!! CONNECTION CAN'T BE ESTABLISHED";
-}
+<?php
+
+try{
+
+	$pdo=new PDO("mysql:host=localhost;port=3306;dbname=onestore","root","");
+
+	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+	$_SESSION['success']="Connected successfully";
+
+}catch(PDOException $e){
+
+	$_SESSION['error']="OOPS !!! CONNECTION CAN'T BE ESTABLISHED";
+
+}
+
 ?>
--- extras/search/pdo.php
+++ extras/search/pdo.php
@@ -1,6 +1,6 @@
 <?php
 try{
-	$pdo=new PDO("mysql:host=localhost;port=3306;dbname=OS","root","");
+	$pdo=new PDO("mysql:host=localhost;port=3306;dbname=onestore","root","");
 	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	$_SESSION['success']="Connected successfully";
 }catch(PDOException $e){
